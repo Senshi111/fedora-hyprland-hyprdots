f1246dd0550542b06a342f450c6da51636d7d490
Bane <40372242+Senshi111@users.noreply.github.com>
2024-01-28 20:08:23 +0100

    Update install_nwg-look.sh

--- build-hyprland-and-apps/install_nwg-look.sh
+++ build-hyprland-and-apps/install_nwg-look.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Clone and install nwg-look
-git clone https://github.com/nwg-piotr/nwg-look.git
-cd nwg-look
-make build
-sudo make install
+#git clone https://github.com/nwg-piotr/nwg-look.git
+#cd nwg-look
+#make build
+#sudo make install
+sudo dnf copr enable -y tofik/nwg-shell 
+sudo dnf install -y nwg-look
 cd ..
