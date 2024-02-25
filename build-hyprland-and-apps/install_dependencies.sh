efcf46611eb562dc1b401190d64cc1a15dffec31
Bane <40372242+Senshi111@users.noreply.github.com>
2024-02-25 12:06:33 -0500

    Update install_dependencies.sh

--- build-hyprland-and-apps/install_dependencies.sh
+++ build-hyprland-and-apps/install_dependencies.sh
@@ -42,4 +42,12 @@ sudo dnf install -y \
             https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
+# Install Google Chrome
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install -y google-chrome-stable
 
+# Install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install -y code
